2374b2ac3cddd25a0b053ff23fefca72598df9ce
Vikas Gaikwad <vgaikwad299@gmail.com>
2024-06-19 09:49:00 +0530

    logic added for friend request and diffrent type of list

---
+++ social_networking/users/handlers/friends_handler.py
@@ -0,0 +1,134 @@
+from django.core.paginator import (
+    Paginator,
+    PageNotAnInteger,
+    EmptyPage
+)
+from django.utils import timezone
+from django.db.models import Q
+
+from datetime import timedelta
+
+from base.exceptions import TooManyRequestsException, BaseAPIException
+from users.choices import FriendRequestStatusChoices
+from users.handlers.user_handler import UserHandler
+from users.models import (
+    FriendRequest,
+    UserProfile,
+    UserFollower
+)
+
+
+class FriendsHandler:
+
+    def get_all_users_on_platform(self, page_num, search_term):
+        profiles = UserProfile.objects.all()
+
+        if search_term and len(search_term) > 2:
+            profiles = profiles.filter(
+                Q(user__email__icontains=search_term) |
+                Q(name__icontains=search_term) |
+                Q(mobile__icontains=search_term)
+            )
+        # pagination
+        paginator = Paginator(profiles, 10)
+        try:
+            paginated_response = paginator.page(page_num)
+        except PageNotAnInteger:
+            paginated_response = paginator.page(1)
+        except EmptyPage:
+            paginated_response = paginator.page(paginator.num_pages)
+
+        response_data = dict()
+        response_data['profiles'] = [
+            UserHandler().generate_profile_response(profile, jwt=False) for profile in
+            paginated_response.object_list]
+        response_data['total_pages'] = paginator.num_pages
+        response_data['current_page'] = int(page_num)
+        return response_data
+
+    def get_all_friends_for_user(self, user, page_num, search_term):
+        user_followers = UserFollower.objects.filter(
+            profile=user.profile
+        )
+
+        if search_term and len(search_term) > 2:
+            user_followers = user_followers.filter(
+                Q(followed_by__user__email__icontains=search_term) |
+                Q(followed_by__name__icontains=search_term) |
+                Q(followed_by__mobile__icontains=search_term)
+            )
+
+        paginator = Paginator(user_followers, 10)
+        try:
+            paginated_response = paginator.page(page_num)
+        except PageNotAnInteger:
+            paginated_response = paginator.page(1)
+        except EmptyPage:
+            paginated_response = paginator.page(paginator.num_pages)
+
+        response_data = dict()
+        response_data['friends'] = [
+            {**UserHandler().generate_profile_response(
+                user_follower.followed_by, jwt=False),
+             'is_close_friend': user_follower.is_close_friend,
+             'followed_date': user_follower.followed_date
+             } for user_follower in
+            paginated_response.object_list]
+        response_data['total_pages'] = paginator.num_pages
+        response_data['current_page'] = int(page_num)
+        return response_data
+
+    def get_all_pending_friend_requests(self, user, page_num, search_term):
+        pending_friend_requests = FriendRequest.objects.filter(
+            from_user=user.profile,
+            status=FriendRequestStatusChoices.PENDING
+        )
+        paginator = Paginator(pending_friend_requests, 10)
+        try:
+            paginated_response = paginator.page(page_num)
+        except PageNotAnInteger:
+            paginated_response = paginator.page(1)
+        except EmptyPage:
+            paginated_response = paginator.page(paginator.num_pages)
+
+        response_data = dict()
+        response_data['pending_requests'] = [
+            UserHandler().generate_profile_response(
+                pending_friend_request.to_user, jwt=False) for pending_friend_request in
+            paginated_response.object_list]
+        response_data['total_pages'] = paginator.num_pages
+        response_data['current_page'] = int(page_num)
+        return response_data
+
+    def send_friend_request(self, user, to_profile_id):
+        to_profile = UserProfile.objects.get(uuid=to_profile_id)
+        one_minute_ago = timezone.now() - timedelta(minutes=1)
+        if FriendRequest.objects.filter(
+                from_user=user.profile, to_user=to_profile).exists():
+            raise BaseAPIException(
+                'Friend request already sent.',
+                'request_already_exists')
+
+        recent_requests_count = FriendRequest.objects.filter(
+            from_user=user.profile,
+            timestamp__gte=one_minute_ago).count()
+
+        if recent_requests_count >= 3:
+            raise TooManyRequestsException(
+                'You can not send more than 3 friend requests within a minute.',
+                'too_many_requests')
+
+        friend_request = FriendRequest.objects.create(
+            from_user=user.profile,
+            to_user=to_profile)
+
+        return UserHandler().generate_profile_response(friend_request.to_user)
+
+    def take_action_on_friend_request(self):
+        pass
+
+    def get_all_block_friends(self):
+        pass
+
+    def get_all_close_friends(self):
+        pass
