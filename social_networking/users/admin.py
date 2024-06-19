884eb73e4a86479d9e2cce9fb00b73c9add3d249
Vikas Gaikwad <vgaikwad299@gmail.com>
2024-06-19 09:50:03 +0530

    new model added to admin panel

--- social_networking/users/admin.py
+++ social_networking/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from base.admin import BaseModelAdmin
-from users.models import UserProfile
+from users.models import UserProfile, UserFollower, FriendRequest
 
 
 @admin.register(UserProfile)
@@ -8,3 +8,19 @@ class UserProfileAdmin(BaseModelAdmin):
     search_fields = ('uuid', 'user__email', 'mobile', 'name', 'is_verified')
     list_display = (
         'uuid', 'user', 'mobile', 'name', 'is_verified')
+
+
+@admin.register(UserFollower)
+class UserFollowerAdmin(BaseModelAdmin):
+    search_fields = (
+        'uuid', 'profile__user__email', 'followed_by__user__email', 'name', 'is_verified')
+    list_display = (
+        'uuid', 'profile', 'followed_by', 'status')
+
+
+@admin.register(FriendRequest)
+class FriendRequestAdmin(BaseModelAdmin):
+    search_fields = (
+        'uuid', 'from_user__user__email', 'to_user__user__email')
+    list_display = (
+        'uuid', 'from_user', 'to_user', 'status')
